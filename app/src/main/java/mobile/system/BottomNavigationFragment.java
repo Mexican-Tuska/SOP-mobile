b02fd623bd212aa5bb740f424252bda3453be9b0
Vladimir Nikulin <mail.jorey@gmail.com>
2022-10-19 22:31:34 +0300

    TODO, FIXME: Доделать переключение нижних кнопок

--- app/src/main/java/mobile/system/BottomNavigationFragment.java
+++ app/src/main/java/mobile/system/BottomNavigationFragment.java
@@ -1,12 +1,25 @@
 package mobile.system;
 
+import static androidx.navigation.Navigation.findNavController;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +73,38 @@ public class BottomNavigationFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bottom_navigation, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        BottomNavigationView navView = view.findViewById(R.id.bottom_nav_view);
+        Log.d("NAV_VIEW", navView == null ? "NULL" : navView.toString());
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_sop,
+                R.id.navigation_schedule,
+                R.id.navigation_attendance,
+                R.id.navigation_home,
+                R.id.navigation_profile)
+                .build();
+
+//        NavController navController = findNavController(root);
+
+        AppCompatActivity context = (AppCompatActivity) getActivity();
+        Log.d("NAV_VIEW", context == null ? "NULL" : context.toString());
+
+//        assert context != null;
+        NavController navController = NavHostFragment.findNavController(this);
+
+
+//        Log.d("NAV_VIEW", navController == null ? "NULL" : navController.toString());
+
+
+        NavigationUI.setupActionBarWithNavController(context, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
